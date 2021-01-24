fa24499d77afee37fce6aa32500a8389dfee66fc
Rareș Cosma <rares@getbetter.ro>
2021-01-24 18:01:34 +0100

    Remove telemetry, infra noise and other crap

--- src/api/github-issues_get.ts
+++
@@ -1,8 +0,0 @@
-import { APIGatewayProxyResult } from "aws-lambda";
-import axios from "axios";
-import { wrapAxios, getGithubOpts } from "../lambda-helpers";
-
-export const handler = async (): Promise<APIGatewayProxyResult> => {
-  const opts = getGithubOpts();
-  return wrapAxios(axios(`https://api.github.com/issues`, opts));
-};
